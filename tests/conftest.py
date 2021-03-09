70687711852a1ed0d5df74a388a01c443e991463
chiragkumarmacwan <SolWise01102411$$$$>
2021-03-09 12:13:44 -0500

    removed unwated commented parts from file:conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,6 @@ import pytest
 
 @pytest.fixture(scope="class")
 def setUp(request, browser):
-    # # application url
-    # baseUrl = "http://www.demo.guru99.com/V4/index.php"
-    #
-    # # browser configuration
-    # driver = webdriver.Chrome(executable_path="/Users/chiragkumarmacwan/Downloads/browserDrivers/chromedriver")
-    # # driver = webdriver.Firefox(executable_path="/Users/chiragkumarmacwan/Downloads/browserDrivers/geckodriver")
-    # driver.maximize_window()
-    # driver.implicitly_wait(5)
-    # driver.set_page_load_timeout(10)
-    # driver.get(baseUrl)
     wdf = WebDriverFactory(browser)
     driver = wdf.getWebDriverInstance()
     if request.cls is not None:
